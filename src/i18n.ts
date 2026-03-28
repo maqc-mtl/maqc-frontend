d3f7cc1d96243026c965eee7238177bd2ca6c992
adai <adai@WIN-WRK-66.siaaccess.com>
2026-03-27 22:22:57 -0400

    init language is fr

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import enTranslation from './locales/en/translation.json';
+// import enTranslation from './locales/en/translation.json';
 import frTranslation from './locales/fr/translation.json';
 import zhTranslation from './locales/zh/translation.json';
 
@@ -11,11 +11,11 @@ i18n
     .use(initReactI18next)
     .init({
         resources: {
-            en: { translation: enTranslation },
+            // en: { translation: enTranslation },
             fr: { translation: frTranslation },
             zh: { translation: zhTranslation }
         },
-        fallbackLng: 'en',
+        fallbackLng: 'fr',
         interpolation: {
             escapeValue: false
         }
