cc03aebf4e389ec58806a8a689d1d7e732d3dbdd
adai <adai@WIN-WRK-66.siaaccess.com>
2026-04-10 23:55:46 -0400

    phrase3

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import type { PropertyScoreDTO } from '../types/PropertyScoreDTO';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    // phrase3 baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'https://maqc-backend.onrender.com/api/v1',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
